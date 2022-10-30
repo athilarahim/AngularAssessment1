e5f53c99f8fcf360e78eaab16020162f59f928d5
Athila Rahim <athilarahim@GMG.LOCAL>
2022-10-30 18:05:56 +0530

    done usecases

--- src/app/main/table/table.component.ts
+++ src/app/main/table/table.component.ts
@@ -7,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  studentArray=[{admno:"1",name:"Athila",dept:"CS"},{admno:"2",name:"Aysha",dept:"Civil"},{admno:"3",name:"Azhar",dept:"CS"},{admno:"4",name:"Hanan",dept:"IT"},{admno:"5",name:"Nanda",dept:"EC"},{admno:"6",name:"Sharron",dept:"civil"},{admno:"7",name:"Akshay",dept:"EC"},{admno:"8",name:"Sriram",dept:"ME"},{admno:"9",name:"Ajay",dept:"IT"},{admno:"10",name:"Aswin",dept:"ME"}]
+  studentArray=[{admno:"1",name:"Athila",dept:"CS"},{admno:"2",name:"Aysha",dept:"Civil"},{admno:"4",name:"Hanan",dept:"IT"},{admno:"6",name:"Sharron",dept:"Civil"},{admno:"7",name:"Akshay",dept:"EC"},{admno:"8",name:"Sriram",dept:"ME"}]
    
- onDoubleClick(){
-  console.log("hiiiiiiiiiiiiiiiiiiiiiii");
+ onDoubleClick(disp:string,value:string){
+  
+  
+  var getval:any= document.getElementById(value) as HTMLInputElement
+  var getid:any=document.getElementById(disp) as HTMLInputElement
+
+  
+ 
+    getval.removeAttribute("hidden")
+
+    getid.setAttribute("hidden",true)
+  
+  
+  
+ }
+ EditValue(insertid:string,curid:string,hidd:string){
+  var insertval:any=(document.getElementById(insertid) as HTMLInputElement)
+  var currid:any=(document.getElementById(curid) as HTMLInputElement)
+  var hiddenid:any=(document.getElementById(hidd) as HTMLInputElement)
+
+  hiddenid.setAttribute("hidden",true)
+
+
+  currid.textContent=insertval.value
+
+  currid.removeAttribute("hidden")
   
  }
 
