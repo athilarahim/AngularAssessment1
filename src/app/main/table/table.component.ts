d16d2e4a79affe81e85981276d7456790b46f86b
Athila Rahim <athilarahim@GMG.LOCAL>
2022-10-31 13:18:48 +0530

    final commit

--- src/app/main/table/table.component.ts
+++ src/app/main/table/table.component.ts
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class TableComponent implements OnInit {
 
 
-  studentArray=[{admno:"1",name:"Athila",dept:"CS",show:false},{admno:"2",name:"Aysha",dept:"Civil",show:false},{admno:"4",name:"Hanan",dept:"IT",show:false},{admno:"6",name:"Sharron",dept:"Civil",show:false},{admno:"7",name:"Akshay",dept:"EC",show:false},{admno:"8",name:"Sriram",dept:"ME",show:false}]
+  studentArray=[{admno:"1",name:"Athila",dept:"CS",show:''},
+  {admno:"2",name:"Aysha",dept:"Civil",show:''},
+  {admno:"4",name:"Hanan",dept:"IT",show:''},
+  {admno:"6",name:"Sharron",dept:"Civil",show:''},
+  {admno:"7",name:"Akshay",dept:"EC",show:''},
+  {admno:"8",name:"Sriram",dept:"ME",show:''},
+  {admno:"3",name:"Nanda",dept:"IT",show:''},
+  {admno:"5",name:"Azhar",dept:"CS",show:''},
+  {admno:"9",name:"Sabari",dept:"ME",show:''},
+  {admno:"10",name:"Arya",dept:"EC",show:''},
+]
   ad=Object.keys(this.studentArray[0])[0]
   nm=Object.keys(this.studentArray[0])[1]
   dp=Object.keys(this.studentArray[0])[2]
-   
+
 
  EditValue(column:any,inputvalue:string,index:any){
 if(column==this.ad)
@@ -21,7 +31,7 @@ else if(column==this.nm)
 index.name=inputvalue
 else if(column==this.dp)
 index.dept=inputvalue
-index.show=false;
+index.show='';
   
  }
 
